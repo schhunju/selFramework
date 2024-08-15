be76bb1e3cc3aa8acddf18e8bc4c0094a881a2f3
ashmitaabasnet <ashmitabasnet2002@gmail.com>
2024-08-15 18:41:10 +0545

    Add screenshot.py to Utilities folder

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -4,6 +4,8 @@ import pytest
 
 from Pages.Basepage import BasePage
 from Pages.LoginPage import LoginPage
+from Utilities import screenshot
+from Utilities.screenshot import ScreenshotUtil
 
 
 class TestLogin(BasePage):
@@ -15,5 +17,9 @@ class TestLogin(BasePage):
             self.login.enterFirstName("John")
             assert "dashboard" in self.driver.title
             time.sleep(5)
+
+            #take screenshot
+            ScreenshotUtil.take_screenshot(self.driver, "login_screenshot.png")
+
         except Exception as e:
             print(str(e))
