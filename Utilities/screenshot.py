862821663979569e4e1e756ed2a0823402e9dd5c
ArchanaSapkota <sapkotaarchana6@gmail.com>
2024-08-21 22:26:06 +0545

    New ss file added

---
+++ Utilities/screenshot.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class ScreenshotUtil:
+
+    @staticmethod
+    def take_screenshot(driver: WebDriver, file_name: str):
+        # Save the screenshot with the provided file name
+        driver.save_screenshot(file_name)
+        print(f"Screenshot saved as {file_name}")
