38dfd506d7a63cf058b11a9a3ad24d92c296ebc3
ArchanaSapkota <sapkotaarchana6@gmail.com>
2024-08-26 11:47:05 +0545

    Email file updated

--- Database/connect_Email.py
+++ Database/connect_Email.py
@@ -2,7 +2,6 @@ import smtplib
 from email.mime.text import MIMEText
 
 try:
-    # Create the email
     msg = MIMEText('Hello!..This is the body of the email.')
     msg['Subject'] = 'Subject of the Email'
     msg['From'] = 'sapkotaarchana6@gmail.com'
@@ -11,13 +10,10 @@ try:
     # Connect to the SMTP server
     server = smtplib.SMTP('smtp.gmail.com', 587)
 
-    # Upgrade the connection to a secure encrypted SSL/TLS connection
     server.starttls()
 
-    # Login to the server
     server.login('sapkotaarchana6@gmail.com', 'wqog vgcj wvdz xidv')
 
-    # Send the email
     server.send_message(msg)
 
 
