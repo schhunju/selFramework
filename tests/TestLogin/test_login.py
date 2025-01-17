3b4ca0758d96732a10e012908fcd3d515a0fe9cd
Sharad Chhunju <chhunjus@gmail.com>
2025-01-17 23:34:54 +0545

    refactored the framework

--- tests/TestLogin/test_login.py
+++ tests/TestLogin/test_login.py
@@ -1,7 +1,6 @@
 import time
 from Pages.Basepage import BasePage
 from Pages.LoginPage import LoginPage
-from Utilities.Logger import Logger
 
 
 class TestLogin(BasePage):
@@ -9,5 +8,4 @@ class TestLogin(BasePage):
     def test_login(self, base_url):
 
         self.login_page = LoginPage(self.driver)
-        self.logger = Logger.get_logger(self)
         self.login_page.search_google(base_url, "abcd")
